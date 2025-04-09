6197eb32f28acf6835c6b165f1ed52964414bea7
neo-the-one786 <119244854+neo-the-one786@users.noreply.github.com>
2025-04-09 20:58:28 +0530

    final commit :)

--- my-portfolio/src/components/SocialIcons.tsx
+++ my-portfolio/src/components/SocialIcons.tsx
@@ -1,17 +1,19 @@
-import {FiGithub, FiLinkedin, FiYoutube} from "react-icons/fi";
+import {FiGithub, FiLinkedin} from "react-icons/fi";
 import Link from "next/link";
-import {SiCodechef, SiCodeforces, SiGeeksforgeeks, SiKaggle, SiLeetcode} from "react-icons/si";
+import {SiCodechef, SiCodeforces, SiGeeksforgeeks, SiHackerrank, SiIndeed, SiKaggle, SiLeetcode, SiStackoverflow} from "react-icons/si";
 
 export default function SocialIcons() {
     const socialIcons = [
         {name: "LinkedIn", icon: <FiLinkedin></FiLinkedin>, link: "https://www.linkedin.com/in/divyanshu-mishra-neo-the-one786/"},
+        {name: "Indeed", icon: <SiIndeed></SiIndeed>, link: "https://profile.indeed.com/p/divyanshum-m23m0mh"},
         {name: "GitHub", icon: <FiGithub></FiGithub>, link: "https://github.com/neo-the-one786"},
-        {name: "YouTube", icon: <FiYoutube></FiYoutube>, link: "https://www.youtube.com/"},
         {name: "Kaggle", icon: <SiKaggle></SiKaggle>, link: "https://www.kaggle.com/neotheone786"},
         {name: "LeetCode", icon: <SiLeetcode></SiLeetcode>, link: "https://leetcode.com/u/neo_the_one786/"},
         {name: "Codeforces", icon: <SiCodeforces></SiCodeforces>, link: "https://codeforces.com/profile/neo_the_one786"},
         {name: "CodeChef", icon: <SiCodechef></SiCodechef>, link: "https://www.codechef.com/users/neo_the_one786"},
+        {name: "Hackerrank", icon: <SiHackerrank></SiHackerrank>, link: "https://www.hackerrank.com/profile/neo_the_one786"},
         {name: "GeeksForGeeks", icon: <SiGeeksforgeeks></SiGeeksforgeeks>, link: "https://www.geeksforgeeks.org/user/neo_the_one786/"},
+        {name: "StackOverflow", icon: <SiStackoverflow></SiStackoverflow>, link: "https://stackoverflow.com/users/21681013/neo-the-one786"},
     ];
     return (
         <>
